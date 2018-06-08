8d97e0c605181938c089f99ef313f4714f8ed101
Aleksandr Krasnov <alek.krasnov@interapthq.com>
2018-06-08 13:48:15 -0400

    Checking Jenkins

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express')
  })
   
  app.listen(3000, function () {
-   console.log('Example app listening on port 3000!')
+   console.log('App on the port 3000!')
  })
   
  module.exports = app;
